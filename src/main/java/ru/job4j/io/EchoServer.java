d59ec1586b8607555f2953c35d8fc3fd64b8aa8a
EfmovMaxim <efimov-m-v@ya.ru>
2022-04-15 22:58:41 +0300

    1. Log4j. Логирование системы

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str =
+                            in.readLine()) {
+                        System.out.println(str);
+                        if (str.contains("?msg=Exit")) {
+                            server.close();
+                            break;
+                        } else if (str.contains("?msg=Hello")) {
+                            //out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello, dear friend.".getBytes());
+                        } else if (str.contains("?msg=Any")) {
+                            //out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("What.".getBytes());
+                        }
+                    }
+                    out.flush();
+                }
+            }
+        }
+    }
+}
