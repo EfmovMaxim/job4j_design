37fb47e4ead488717d7f36e0b189e80b54fe1ffc
EfmovMaxim <efimov-m-v@ya.ru>
2022-04-17 22:55:54 +0300

    4. JAXB. Преобразование XML в POJO

--- src/main/java/ru/job4j/serialization/Contact.java
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -3,15 +3,28 @@ package ru.job4j.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
 import java.io.*;
 import java.nio.file.Files;
 
+@XmlRootElement(name = "contact")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Contact implements Serializable {
     private static final long serialVersionUID = 1L;
-    private final int zipCode;
-    private final String phone;
-    private final String[] emails;
-    private final Person person;
+    @XmlAttribute
+    private int zipCode;
+    @XmlAttribute
+    private String phone;
+    @XmlElementWrapper
+    @XmlElement(name = "email")
+    private String[] emails;
+    private Person person;
+
+    public Contact() {
+        this(0, "", new String[]{}, new Person());
+    }
 
     public Contact(int zipCode, String phone, String[] emails, Person person) {
         this.zipCode = zipCode;
@@ -19,6 +32,7 @@ public class Contact implements Serializable {
         this.emails = emails;
         this.person = person;
     }
+
 
     public int getZipCode() {
         return zipCode;
@@ -36,7 +50,7 @@ public class Contact implements Serializable {
                 + '}';
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
         final Contact contact = new Contact(123456, "+7 (111) 111-11-11",
                 new String[]{"1@mail.ru", "2@yandex.ru"}, new Person("Ivan", "Ivanovich", 23));
         /* Запись объекта во временный файл, который удалится системой */
@@ -56,5 +70,19 @@ public class Contact implements Serializable {
 
         Gson gson = new GsonBuilder().create();
         System.out.println("JSON:" + gson.toJson(contact));
+
+
+        JAXBContext context = JAXBContext.newInstance(Contact.class);
+        Marshaller marshaller = context.createMarshaller();
+
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        String xml = "";
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(contact, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println("XML:\n" + xml);
+        }
     }
 }
